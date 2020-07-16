9777f1498709fb541f1a2f17a0b0fabfb19a57d4
dudu3000 <49913163+dudu3000@users.noreply.github.com>
2020-07-16 15:16:53 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <main>
+      <input type="text" id="username" class="textArea text" placeholder="Username">
+      <input type="password" id="pw" class="textArea text" placeholder="Password">
+    </main>
+  </div>
+</template>
+
+
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      url_base:'localhost:3000/user/login',
+      query: '',
+      response: ''
+    }
+  },
+  methods: {
+    fetchLogin (e) {
+      if(e.key == "Enter") {
+        fetch(`${this.url_base}`)
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+.text{
+  width: 100%;
+  font-size: 25px;
+  border: 2px solid #000000;
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.75);
+  padding:10px;
+  margin-top:25px;
+
+}
+
+
+main{
+  display: flex;
+  align-content: center;
+  width:50%;
+  flex-direction: column;
+}
+</style>
